1774a6579c07dfd82e48827ef9b9e1f77c09fb9c
steven21029 <78768044+steven21029@users.noreply.github.com>
2025-07-28 12:57:20 -0600

    1

--- src/data/accesorios.js
+++ src/data/accesorios.js
@@ -1,5 +1,6 @@
 const accesorios = [
     {
+      id: 1,
       nombre: "gorjana de corazón",
       descripcion: "gorjana Collar con corazón Parker para mujer, Chapado en oro, No es una piedra preciosa.",
       precio: 610,
@@ -7,6 +8,7 @@ const accesorios = [
       cantidad: 1,
     },
     {
+      id: 1,
       nombre: "Pulsera Swarovski",
       descripcion: "Swarovski Colección de pulseras de tenis de cristal angelical.",
       precio: 1500,
@@ -14,6 +16,7 @@ const accesorios = [
       cantidad: 3,
     },
     {
+      id: 1,
       nombre: "Breguet reloj lujo",
       descripcion: "Fragancia floral suave con notas de jazmín y vainilla.",
       precio: 2500,
@@ -21,6 +24,7 @@ const accesorios = [
       cantidad: 3,
     },
     {
+      id: 1,
       nombre: "Hawkers One negras",
       descripcion: "Estas gafas de sol son uno de los productos más vendidos de la marca española Hawkers, y no es para menos. Las Hawkers One son muy versátiles y se adaptan perfectamente a cualquier persona.",
       precio: 150,
--- src/data/bases.js
+++ src/data/bases.js
@@ -2,6 +2,7 @@
 
 export const bases = [
   {
+    id: 1,
     nombre: "Clinique Even Better",
     descripcion: "Maquillaje potenciado con la tecnología de 3 sérums: ácido hialurónico, puede mejorar notablemente nuestro cutis",
     precio: 610,
@@ -9,6 +10,7 @@ export const bases = [
     alt: "Bases para Dama 1"
   },
   {
+    id: 1,
     nombre: "Colorstay Makeup",
     descripcion: "Base está especialmente diseñada para pieles mixtas y grasas, ya que este tipo de cutis son los que más sufren a la hora de escoger un maquillaje adecuado que cuide su piel al mismo tiempo que ofrezca una cobertura y un acabado bonitos.",
     precio: 600,
@@ -16,6 +18,7 @@ export const bases = [
     alt: "Bases para Dama 2"
   },
   {
+    id: 1,
     nombre: "L'Oréal Paris Accord",
     descripcion: "Sus pigmentos son invisibles al ojo y permiten unificar el tono y obtener un resultado natural, su formulación incluye cuatro aceites volátiles que se funden con la piel y añade un 75% de crema hidratante para añadir más confort.",
     precio: 500,
@@ -23,6 +26,7 @@ export const bases = [
     alt: "Bases para Dama 3"
   },
   {
+    id: 1,
     nombre: "Estée Lauder Double",
     descripcion: "Su fórmula ligera y sensible para la piel y su acabado natural de larga duración ideal para las pieles mixtas y grasas, ya que es una de las mejores a la hora de controlar, de verdad, los brillos derivados del exceso de grasa.",
     precio: 700,
